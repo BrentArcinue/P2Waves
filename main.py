e15b5428d8e8274e0f6906f54e2dc02c8c8cf4b3
Brent Arcinue <brentarcinuegithub@gmail.com>
2020-12-17 09:46:02 -0800

    sample login page (not complete yet)

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# home screen
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host = '127.0.0.1',port='3000')
