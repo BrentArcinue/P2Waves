6e93149f54b3655456499ac8bc9ebb1ea0312f5d
lukemanning244 <72890051+lukemanning244@users.noreply.github.com>
2020-12-18 15:41:07 -0800

    port test

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def home():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host = '127.0.0.1',port='3000')
+    app.run(debug=True, host = '127.0.0.1',port='3001')
