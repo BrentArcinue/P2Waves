b394cf1cdbc28b3997712599183a94e92af65217
lukemanning244 <72890051+lukemanning244@users.noreply.github.com>
2021-01-11 09:38:48 -0800

    changed login html code, added static folder, changed home page

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 # home screen
 @app.route("/home")
-@app.route("/")
 def home():
+    return render_template("home.html")
+@app.route("/")
+def login():
     return render_template("login.html")
-
 if __name__ == "__main__":
     app.run(debug=True, host = '127.0.0.1',port='3001')
