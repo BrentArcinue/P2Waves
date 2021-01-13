f595395510bf42af88a0d7430e07b60b15a8a0f4
Cody <hotcococody@gmail.com>
2021-01-13 10:11:02 -0800

    Approuted the game pages and made a template for each page

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template("home.html")
+
 @app.route("/")
 def login():
     return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True, host = '127.0.0.1',port='3001')
+
+@app.route('/project/game1')
+def game1_route():
+    return render_template("game1.html")
+
+@app.route('/project/game2')
+def game2_route():
+    return render_template("game2.html")
